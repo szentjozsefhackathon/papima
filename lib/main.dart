76200f4970cf98203071c2de120c190f1a153ed1
Vörös László <vorosl@inf.u-szeged.hu>
2024-12-22 20:11:02 +0100

    Fix image CORS

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,83 @@ import 'package:url_launcher/url_launcher.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
+import 'dart:ui_web' as ui_web;
+import 'package:web/web.dart' as web;
 void main() {
   runApp(PapImaApp());
+}
+
+class DynamicHtmlImage extends StatefulWidget {
+  final String src; // Képforrás
+  final double maxWidth; // Maximális szélesség
+  final double maxHeight; // Maximális magasság
+
+  const DynamicHtmlImage({
+    Key? key,
+    required this.src,
+    required this.maxWidth,
+    required this.maxHeight,
+  }) : super(key: key);
+
+  @override
+  _DynamicHtmlImageState createState() => _DynamicHtmlImageState();
+}
+
+class _DynamicHtmlImageState extends State<DynamicHtmlImage> {
+  late web.HTMLImageElement _imageElement; // HTML ImageElement a kép megjelenítéséhez
+  late String _viewType; // Egyedi nézettípus az azonosításhoz
+
+  @override
+  void initState() {
+    super.initState();
+    _viewType = 'html-image-view-${UniqueKey()}'; // Egyedi azonosító generálása
+
+    // Regisztráljuk a platformnézet gyárat
+    if (kIsWeb) {
+      // ignore: undefined_prefixed_name
+      ui_web.platformViewRegistry.registerViewFactory(
+        _viewType,
+        (int viewId) => _imageElement, // A HTML elem visszaadása
+      );
+    }
+
+    // HTML ImageElement inicializálása
+    _imageElement = web.HTMLImageElement()
+      ..src = widget.src // Beállítjuk a képforrást
+      ..style.objectFit = 'scale-down'
+      ..style.maxWidth = '${widget.maxWidth}px' // Maximális szélesség stílus
+      ..style.maxHeight = '${widget.maxHeight}px'; // Maximális magasság stílus
+  }
+
+  @override
+  void didUpdateWidget(DynamicHtmlImage oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    // Ellenőrizzük, hogy változott-e a képforrás vagy a méretek
+    if (widget.src != oldWidget.src ||
+        widget.maxWidth != oldWidget.maxWidth ||
+        widget.maxHeight != oldWidget.maxHeight) {
+      setState(() {
+        _imageElement.src = widget.src; // Új képforrás beállítása
+        _imageElement.style.maxWidth = '${widget.maxWidth}px'; // Új max szélesség
+        _imageElement.style.maxHeight = '${widget.maxHeight}px'; // Új max magasság
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        SizedBox(
+          width: widget.maxWidth, // A nézet szélessége
+          height: widget.maxHeight, // A nézet magassága
+          child: HtmlElementView(viewType: _viewType), // HTML nézet
+        ),
+      ],
+    );
+  }
 }
 
 class PapImaApp extends StatelessWidget {
@@ -197,7 +272,13 @@ class _PapImaHomePageState extends State<PapImaHomePage> {
                     children: [
                       if (currentPriest != null) ...[
                         if (currentPriest['img'] != null) ...[
-                          Image.network(
+                          kIsWeb ? 
+                          DynamicHtmlImage(
+                            src: currentPriest['img'],
+                            maxWidth: 300,
+                            maxHeight: 300,
+                          )
+                          : Image.network(
                             currentPriest['img'],
                             width: 300,
                             height: 300,
