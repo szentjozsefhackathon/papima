e875378483f90f5dfa54eadaf65f27a3fe2338d0
Vörös László <vorosl@inf.u-szeged.hu>
2025-01-02 10:01:47 +0100

    15. dailyCounter bugfix, prayer repo

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "823737804"
+    serviceWorkerVersion: "3753017777"
   }
 });
