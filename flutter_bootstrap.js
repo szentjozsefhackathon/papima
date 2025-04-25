16b5557e979a555feaa6f0674c2f066eb2539462
Vörös László <vorosl@inf.u-szeged.hu>
2025-04-25 16:54:52 +0200

    7+25 Cardinals

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1408043487"
+    serviceWorkerVersion: "1960409072"
   }
 });
